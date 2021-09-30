e3634cbfc3b5e7ba96ddbec8abf005aed447f652
bokuore <6408741@qq.com>
2021-09-30 17:26:17 +0800

    add stub for snapshot

--- daemon.go
+++ daemon.go
@@ -60,6 +60,11 @@ func (service *MainService)Stop() (output string, err error){
 	return
 }
 
+func (service *MainService) Snapshot() (output string, err error){
+	output ="hello, this is stub for snapshot"
+	return
+}
+
 func generateConfigure(workingPath string) (err error){
 	if err = configureNetworkForCell(); err != nil{
 		fmt.Printf("configure cell network fail: %s\n", err.Error())
